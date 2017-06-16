293b6b917b36bae7c4d00c3182d043cf73c14c16
chenjin <chenjin@dongfangfuli.com>
2017-06-16 18:19:13 +0800

    update

--- js/hash.js
+++ js/hash.js
@@ -63,56 +63,127 @@ var Hashes = function (ele) {
         if (targetParentElement.length == 0) {
             console.error("目标元素父节点不存在");
             this.lists = hashes;
-            return ;
+            return;
         }
 
         var scrollBoxElement = $("[data-scroll-box=" + targetParentElement.data("scroll-target") + "]");
         var targetElements = targetParentElement.children();
         var scrollElements = scrollBoxElement.children();
-
+        // console.info(targetElements);
+        // console.info(scrollElements);
         //若目标元素不存在
-        if ( targetElements.length == 0
+        if (targetElements.length == 0
             || scrollBoxElement.length == 0
             || targetElements.length != scrollElements.length
         ) {
             console.error("目标元素父节点不存在或匹配长度不对等");
             this.lists = hashes;
-            return ;
+            return;
         }
-
-        for (var key = 0; key < targetElements.length; key++) {
-            var hashKey = $(targetElements[key]).data('id');
-            var hashElement = $(scrollElements[key]);
+        targetElements.each(function (index, e) {
+            var hashKey = $(e).data('id');
+            var hashElement = scrollBoxElement.find("[data-scroll-index="+ hashKey +"]");
             var prevAll = hashElement.prevAll();
             var element = {
-                top : prevAll.length ? prevAll.height() : 0,
-                bottom : prevAll.length ? parseFloat(prevAll.height()) + parseFloat(hashElement.height()) : hashElement.height(),
+                top: prevAll.length ? prevAll.outerHeight(true): 0,
+                bottom: prevAll.length ? parseFloat(prevAll.outerHeight(true)) + parseFloat(hashElement.height()) : hashElement.height(),
+                height: hashElement.innerHeight(),
+                offsetTop: hashElement.offset().top,
             };
 
             hashes[hashKey] = element;
-        }
-            this.lists = hashes;
+
+        });
+
+        /**
+         * 
+         * @type {Array}
+         */
+        // for (var key = 0; key < targetElements.length; key++) {
+        //     var hashKey = $(targetElements[key]).data('id');
+        //     var hashElement = $(scrollElements[key]);
+        //     var prevAll = hashElement.prevAll();
+        //     var element = {
+        //         top: prevAll.length ? prevAll.height() : 0,
+        //         bottom: prevAll.length ? parseFloat(prevAll.height()) + parseFloat(hashElement.height()) : hashElement.height(),
+        //         innerHeight: hashElement.innerHeight(),
+        // offsetTop: hashElement.offset().top,
+        //     };
+        //
+        //     hashes[hashKey] = element;
+        // }
+
+        // var boxElement = {
+        //     top: scrollBoxElement.offset().top,
+        //     bottom: scrollBoxElement.offset().top + scrollBoxElement.height(),
+        //     innerHeight: scrollBoxElement.innerHeight(),
+        //     scrollHeight: scrollBoxElement[0].scrollHeight,
+        // };
+        // hashes.push(boxElement);
+        this.lists = hashes;
     };
 
+    /**
+     * 关系：positionTop(相对父节点的高度)  = scrollTop(滚动条的值) + Top(当前元素所在位置距离起点的位置
+     *
+     * @param scrollTop
+     * @returns {*}
+     */
     this.betweenHashes = function (scrollTop) {
         var hashes = this.lists;
-
+        var res = null;
         for (var key in hashes) {
-            if (hashes[key].top <= scrollTop
-                &&  hashes[key].bottom >= scrollTop
+            var scrollElement = $("[data-scroll-index="+ key +"]");
+            var positionTop = scrollElement.position().top;
+            var positionBottom = positionTop + hashes[key].bottom;
+
+            if (positionTop <= 0
+            && positionBottom >=0
             ) {
-                return key;
+                res = key;
+                break;
+            }
+
+        }
+        return res;
+    };
+
+    /**
+     * 第二种算法  利用滚动条的高度来计算所在位置
+     *
+     *
+     *
+     * @param scrollTop
+     */
+    this.betweenHashes2 = function (scrollTop) {
+        var hashes = this.lists;
+        var res = null;
+        for (var key in hashes) {
+            //相对高度
+            var positionTop = parseInt(hashes[key].offsetTop) - scrollTop;
+            var positionBottom = positionTop + hashes[key].bottom;
+            if ( positionTop <= 0
+                && positionBottom >= 0
+            ) {
+                res = key;
+                break;
             }
         }
-        console.info(scrollTop + "=>" + key);
-        return null;
+        return res;
     };
 
     this.create();
 };
 
-
-
-
+//Jquery计算相对父节点位置的源码，简单化为：offset().top
+var position =function () {
+    if (this[0]) {
+        var e, t, n = {top: 0, left: 0}, r = this[0];
+        return "fixed" === x.css(r, "position") ? t = r.getBoundingClientRect() : (e = this.offsetParent(), t = this.offset(), x.nodeName(e[0], "html") || (n = e.offset()), n.top += x.css(e[0], "borderTopWidth", !0), n.left += x.css(e[0], "borderLeftWidth", !0)), {
+            top: t.top - n.top - x.css(r, "marginTop", !0),
+            left: t.left - n.left - x.css(r, "marginLeft", !0)
+        }
+    }
+};
 
 
--- js/scroll2.js
+++ js/scroll2.js
@@ -2,33 +2,77 @@
  * Created by jim on 2017/6/15.
  */
 var Hashes = new Hashes('proTab0');
+var eee = $("[data-scroll-box=" + $("#proTab0").data("scroll-target") + "]");
 console.info(Hashes);
+console.info(eee.height());
+console.info(eee[0].scrollHeight);
+
 $(document).ready(function () {
     // if (! Hashes) {
     //     var Hashes = new Hashes('proTab0');
     // }
+    var firstChild = eee.children().first();
+    var secondChild = firstChild.next();
+    var thirdChild = secondChild.next();
+    var fourthChild = thirdChild.next();
+    var lastChild = eee.children().last();
+    var lastSecondChild = lastChild.prev();
+    var lastThirdChild = lastSecondChild.prev();
 
     $(".TabContent").scroll(function () {
+        console.info(eee.scrollTop() );
+        console.info('第1个元素:'+ firstChild.text() +'的窗口顶部距离：' + firstChild.offset().top
+            + ",本来高度：" + firstChild.innerHeight() + ", 相对于父元素的高度top:" + firstChild.position().top
+            + ",相对于当前元素底部边界的高度：" + (parseFloat(firstChild.position().top) + parseFloat(firstChild.height()))
+        );
+        console.info('第2个元素:'+ secondChild.text() +'的窗口顶部距离：' + secondChild.offset().top
+            + ",本来高度：" + secondChild.innerHeight() + ", 相对于父元素的高度top:" + secondChild.position().top
+            + ",相对于当前元素底部边界的高度：" + (parseFloat(secondChild.position().top) + parseFloat(secondChild.height()))
+        );
+        console.info('第3个元素: ... 的窗口顶部距离：' + thirdChild.offset().top
+            + ",本来高度：" + thirdChild.innerHeight() + ", 相对于父元素的高度top:" + thirdChild.position().top
+            + ",相对于当前元素底部边界的高度：" + (parseFloat(thirdChild.position().top) + parseFloat(thirdChild.height()))
+        );
+        console.info('第4个元素: ... 的窗口顶部距离：' + fourthChild.offset().top
+            + ",本来高度：" + fourthChild.innerHeight() + ", 相对于父元素的高度top:" + fourthChild.position().top
+            + ",相对于当前元素底部边界的高度：" + (parseFloat(fourthChild.position().top) + parseFloat(fourthChild.height()))
+        );
+        console.info('......');
+
+        console.info('倒数第3个元素:...的窗口顶部距离：' + lastThirdChild.offset().top
+            + ",本来高度：" + lastThirdChild.innerHeight() + ", 相对于父元素的高度top:" + lastThirdChild.position().top
+            + ",相对于当前元素底部边界的高度：" + (parseFloat(lastThirdChild.position().top) + parseFloat(lastThirdChild.height()))
+        );
+        console.info('倒数第2个元素:...的窗口顶部距离：' + lastSecondChild.offset().top
+            + ",本来高度：" + lastSecondChild.innerHeight() + ", 相对于父元素的高度top:" + lastSecondChild.position().top
+            + ",相对于当前元素底部边界的高度：" + (parseFloat(lastSecondChild.position().top) + parseFloat(lastSecondChild.height()))
+        );
+
+        console.info('倒数第一个元素:'+ lastChild.text() +'的窗口顶部距离：' + lastChild.offset().top
+            + ",本来高度：" + lastChild.innerHeight() + ", 相对于父元素的高度top:" + lastChild.position().top
+            + ",相对于当前元素底部边界的高度：" + (parseFloat(lastChild.position().top) + parseFloat(lastChild.height()))
+        );
         var dom = $(this);
         var scrollBox = $(this).data("scroll-box");
         var currentScrollTop = $(this).scrollTop();
         var targetParentElement = $("[data-scroll-target= "+ scrollBox + "]");
         var className = "active";
 
-        var changeTargetKey = Hashes.betweenHashes(currentScrollTop);
+        var changeTargetKey = Hashes.betweenHashes2(currentScrollTop);
 
         this.changeTargetAction = function () {
-            if (initial.positionTop()) {
-                //滚动到顶部时，停在第一个元素处
-                initial.targetFirstAddClass();
-            }
-            else if (initial.endBottom()) {
-                //滚动到底部时，停在最后个元素处
-                initial.targetLastAddClass();
-            } else {
-                //自动完成修改
-                initial.targetAddClass();
-            }
+            // if (initial.positionTop()) {
+            //     //滚动到顶部时，停在第一个元素处
+            //     initial.targetFirstAddClass();
+            // }
+            // else if (initial.endBottom()) {
+            //     //滚动到底部时，停在最后个元素处
+            //     initial.targetLastAddClass();
+            // } else {
+            //     //自动完成修改
+            //     initial.targetAddClass();
+            // }
+            initial.targetAddClass();
         };
 
         var initial = {
@@ -71,7 +115,8 @@ $(document).ready(function () {
                 return dom.scrollTop() == 0;
             },
             endBottom: function () {
-                return (parseFloat(dom.scrollTop()) + parseFloat(dom.height())) >= parseFloat(dom[0].scrollHeight);
+                var end = (parseFloat(dom.scrollTop()) + parseFloat(dom.height())) >= parseFloat(dom[0].scrollHeight);
+                return end;
             },
         };
 
